04e887afb55cba8c43b626d8d8c92226ad23fbdb
bfsoares <54894230+bfsoares@users.noreply.github.com>
2025-12-12 00:46:01 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version="0.1.0",
     packages=['tdbinlog_reader'],
     install_requires=['tgcrypto'],
-    entry_points={
-        'console_scripts': ['tdbinlog_reader=main']
-    }    
+    #entry_points={
+    #    'console_scripts': ['tdbinlog_reader=main']
+    #}    
     # Automatically finds packages in your directory structure
     #packages=find_packages(include=["tdbinlog_reader", "tdbinlog_reader.*"]),
 )
